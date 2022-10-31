87ecf862de9bda2c8fd7a10365a4cb83baf4f4c0
Nick Dalalelis <ndalalelis@microsoft.com>
2022-10-30 22:17:55 -0400

    demo creating a virtual machine

--- demo.sh
+++ demo.sh
@@ -24,7 +24,7 @@ DEMO_PROMPT="${BLACK}➜ ${GREEN}\W "
 # hide the evidence
 clear
 
-p ">>> Verify azure-cli <<<"
+p ">>> Verify azure-cli is installed <<<"
 pe "az --version"
 
 p ">>> Login to Azure <<<"
@@ -36,12 +36,5 @@ pe "az group create -l eastus -n nick-az-cli-demo-rg"
 p ">>> Display the resource group <<<"
 pe "az group show --name nick-az-cli-demo-rg"
 
-p ">>> Deploy a static web app from https://github.com/nickdala/azure-cli-demo <<<"
-pe "az staticwebapp create --name my-first-static-web-app \
-    --resource-group nick-az-cli-demo-rg \
-    --source https://github.com/nickdala/azure-cli-demo \
-    --location eastus2 \
-    --branch main \
-    --app-location \"/\"  \
-    --output-location \"build\" \
-    --login-with-github"
+p ">>> Create a default Ubuntu VM <<<"
+pe "az vm create -n MyVm -g nick-az-cli-demo-rg --image UbuntuLTS --location westus --generate-ssh-keys"
